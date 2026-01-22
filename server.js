5866e1bab4b262816da5b2c58d9db4105bf2ef42
One EV KH <oneevkh@gmail.com>
2026-01-22 11:50:38 +0700

    Setup Node.js for Vercel

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const express = require("express");
-
 const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
 const cors = require("cors");
-const app = express();
 const jwt = require("jsonwebtoken");
 
-app.use(cors());
-app.use(express.json());
-app.use(helmet());
-app.use(express.json({ limit: "1kb" }));
-app.use(express.json());
+// Create express app
+const app = express();
 
+// Middlewares
+app.use(cors());
+app.use(express.json({ limit: "1kb" }));
+app.use(helmet());
+
+// Example route
 app.get("/", (req, res) => {
   res.json({
     message: "✅ Server MrBlack is connected! Working as well!",
@@ -20,7 +21,5 @@ app.get("/", (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 8888;
-app.listen(PORT, () => {
-  console.log(`Local: http://localhost:${PORT}`);
-});
+// Export as Vercel serverless function
+module.exports = app;
